4ae819cfc37917b32a411370bd5844b1b14be9f6
PeaceLaced <peacelaced@gmail.com>
2022-02-14 04:25:45 -0500

    Add files via upload
    - too many changes to list
    - added misc for some stream extras

---
+++ twit/twit_api/config/config_twitch[public].py
@@ -0,0 +1,48 @@
+"""
+- Settings for Twitch.
+
+"""
+
+##############################################################################
+#######    Twitch Credentials, each bot has a unique set              ########
+#######    --------------------------------------------------------   ########
+##############################################################################
+
+##############################################################################
+###                                                                        ###
+###   Data for PeaceLaced                                                  ###
+###                                                                        ###
+USER_ID = 'XXXXXXXX'                                                       ###
+ROOM_LIST = ['peacelaced', 'twit_champ']                                   ###
+SUPER_USER = {'peacelaced', 'twit_chart', 'twit_champ', 'twit_chant'}      ###
+##############################################################################
+
+##############################################################################
+###                                                                        ###
+###   ACCESS_TOKEN           https://dev.twitch.tv/console/apps            ###
+###   Twitch Acct.           twit_chart                                    ###
+###                                                                        ###
+ART_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'                               ###
+ART_REFRESH = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'         ###
+ART_CLIENT_ID = ''                           ###
+##############################################################################
+
+##############################################################################
+###                                                                        ###
+###   ACCESS_TOKEN           https://dev.twitch.tv/console/apps            ###
+###   Twitch Acct.           twit_champ                                    ###
+###                                                                        ###
+AMP_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'                               ###
+AMP_REFRESH = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'         ###
+AMP_CLIENT_ID = ''                           ###
+##############################################################################
+
+##############################################################################
+###                                                                        ###
+###   ACCESS_TOKEN           https://dev.twitch.tv/console/apps            ###
+###   Twitch Acct.           twit_chant                                    ###
+###                                                                        ###
+ANT_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'                               ###
+ANT_REFRESH = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'         ###
+ANT_CLIENT_ID = ''                           ###
+##############################################################################
---
+++ twit/twit_commands/api_command.py
@@ -0,0 +1,11 @@
+"""
+- about command API.
+
+"""
+def run_command_command(command, player, args):
+ 
+    return ['!info, !command, !trade, !transfer, ' +
+            '!stats, !value, !balance, ' +
+            '!foss, !mic, !factorio, !ad, ' +
+            '!end and !win. ' +
+            'See the TWIT Game Commands panel for descriptions and arguments.']
